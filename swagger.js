295c2456dcac991a2ea2262587bc5fff784660e3
Gonzalo Coronado <jgcoronadocueva@outlook.com>
2025-11-23 01:14:20 -0500

    Done

--- swagger.js
+++ swagger.js
@@ -6,10 +6,17 @@ const swagger = swaggerAutogen({ openapi: "3.0.0" });
 const doc = {
   info: {
     title: 'Chat with Youtube Videos',
-    description: 'API for chatting with youtube videos',
+    description: 'API for chatting with YouTube videos',
   },
   servers: [
     { url: 'http://localhost:3000', description: 'Local server' },
+  ],
+  tags: [
+    { name: 'Users', description: 'Operations about users' },
+    { name: 'Projects', description: 'Operations about projects' },
+    { name: 'Chats', description: 'Operations about chats' },
+    { name: 'Videos', description: 'Operations about videos' },
+    { name: 'Channels', description: 'Operations about channels' },
   ],
 };
 
