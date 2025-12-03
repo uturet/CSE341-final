93119d514e2f73952591e017511a99f25e415192
Gonzalo Coronado <jgcoronadocueva@outlook.com>
2025-12-03 12:48:53 -0500

    Fixed imports in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import connectDB from "./db/connection";
+import connectDB from "./db/connection.js";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
